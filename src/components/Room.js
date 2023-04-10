775dd8925bf415e0ef8edaeb17745f24663a1b87
HyunJoonKang <steady.kang27@gmail.com>
2023-04-10 12:15:50 +0900

    feat: 손동작 인식, 손동작 시각화

--- src/components/Room.js
+++ src/components/Room.js
@@ -1,6 +1,100 @@
+import { useEffect, useRef, useState } from "react";
 import styled from "styled-components";
+import { drawLandmarks, drawConnectors } from "@mediapipe/drawing_utils";
+import { FilesetResolver, GestureRecognizer } from "@mediapipe/tasks-vision";
+import { HAND_CONNECTIONS } from "@mediapipe/hands";
 
 export default function Room() {
+  const videoRef = useRef(null);
+  const videoCanvasRef = useRef(null);
+  const paperCanvasRef = useRef(null);
+  const [gestureRecognizer, setGestureRecognizer] = useState();
+
+  const getUserCamera = () => {
+    navigator.mediaDevices
+      .getUserMedia({
+        video: true,
+      })
+      .then((stream) => {
+        let video = videoRef.current;
+
+        video.srcObject = stream;
+
+        video.play();
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  useEffect(() => {
+    getUserCamera();
+  }, [videoRef]);
+
+  useEffect(() => {
+    async function run() {
+      const vision = await FilesetResolver.forVisionTasks(
+        "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@latest/wasm"
+      );
+      const result = await GestureRecognizer.createFromOptions(vision, {
+        baseOptions: {
+          modelAssetPath:
+            "https://storage.googleapis.com/mediapipe-tasks/gesture_recognizer/gesture_recognizer.task",
+        },
+        runningMode: "VIDEO",
+        numHands: 2,
+      });
+
+      setGestureRecognizer(result);
+    }
+
+    run();
+  }, []);
+
+  useEffect(() => {
+    const videoCtx = videoCanvasRef.current.getContext("2d");
+
+    function renderLoop() {
+      if (!gestureRecognizer) return;
+
+      if (videoRef.current && videoRef.current.readyState === 4) {
+        let nowInMs = Date.now();
+        const gestureRecognitionResult = gestureRecognizer.recognizeForVideo(
+          videoRef.current,
+          nowInMs
+        );
+
+        videoCtx.save();
+        videoCtx.clearRect(0, 0, videoCanvasRef.width, videoCanvasRef.height);
+
+        const videoWidth = videoRef.current.clientWidth;
+        const videoHeight = videoRef.current.clientHeight;
+
+        videoRef.current.width = videoWidth;
+        videoRef.current.height = videoHeight;
+        videoCanvasRef.current.width = videoWidth;
+        videoCanvasRef.current.height = videoHeight;
+
+        if (gestureRecognitionResult.landmarks.length > 0) {
+          for (const landmarks of gestureRecognitionResult.landmarks) {
+            drawConnectors(videoCtx, landmarks, HAND_CONNECTIONS, {
+              color: "#00FF00",
+              lineWidth: 1,
+            });
+            drawLandmarks(videoCtx, landmarks, {
+              color: "#FF0000",
+              lineWidth: 1,
+            });
+          }
+        }
+        videoCtx.restore();
+      }
+      requestAnimationFrame(renderLoop);
+    }
+
+    requestAnimationFrame(renderLoop);
+  }, [gestureRecognizer]);
+
   return (
     <Wrapper>
       <LeftContainer>
@@ -13,44 +107,14 @@ export default function Room() {
           <Circle diameter="10" color="black" />
         </ToolBox>
         <div>Draw</div>
-        <div>
-          <video
-            style={{
-              position: "absolute",
-              left: 0,
-              right: 0,
-              textAlign: "center",
-              zindex: 9,
-              width: 320,
-              height: 240,
-            }}
-            autoPlay
-          />
-          <canvas
-            style={{
-              position: "absolute",
-              left: 0,
-              right: 0,
-              textAlign: "center",
-              zindex: 9,
-              width: 320,
-              height: 240,
-            }}
-          />
+        <div position="relative">
+          <Video ref={videoRef} autoPlay />
+          <VideoCanvas ref={videoCanvasRef} />
         </div>
       </LeftContainer>
       <RightContainer>
         <Paper></Paper>
-        <canvas
-          style={{
-            position: "absolute",
-            top: 50,
-            left: 50,
-            zindex: 9,
-            width: 960,
-            height: 540,
-          }}
-        />
+        <PaperCanvas ref={paperCanvasRef} />
       </RightContainer>
     </Wrapper>
   );
@@ -89,15 +153,41 @@ const RightContainer = styled.div`
   padding: 50px;
 `;
 
-const ToolBox = styled.div`
-  display: flex;
-  flex-wrap: wrap;
+const Video = styled.video`
+  position: absolute;
+  left: 0;
+  right: 0;
+  z-index: 9;
+  width: 320px;
+  height: 240px;
+`;
+
+const VideoCanvas = styled.canvas`
+  position: absolute;
+  left: 0;
+  right: 0;
+  z-index: 9;
+  width: 320px;
+  height: 240px;
 `;
 
 const Paper = styled.div`
-  position: relative;
+  position: absolute;
   width: 960px;
   height: 540px;
   background-color: white;
   box-shadow: 10px 10px grey;
+`;
+
+const PaperCanvas = styled.canvas`
+  position: absolute;
+  top: 50;
+  left: 50;
+  z-index: 9;
+  width: 960px;
+  height: 540px;
+`;
+const ToolBox = styled.div`
+  display: flex;
+  flex-wrap: wrap;
 `;
