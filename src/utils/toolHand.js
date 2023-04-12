3e31825c888ab84260d0ab6c3bd4b92fdb3acde4
HyunJoonKang <steady.kang27@gmail.com>
2023-04-12 21:18:25 +0900

    fix: 손동작 분기처리 수정, 지우개 기능 수정

--- src/utils/toolHand.js
+++ src/utils/toolHand.js
@@ -1,14 +1,19 @@
-let prevCount = 0;
-let nextCount = 0;
+let count = 0;
+let gesture = "";
 const color = ["red", "orange", "yellow", "blue", "green", "black"];
 let index = 0;
 
 export function prevColor(setColor) {
-  prevCount++;
+  if (gesture === "Thumb_Down") {
+    count++;
+  } else {
+    gesture = "Thumb_Down";
+    count = 0;
+  }
 
-  if (prevCount > 20) {
+  if (count > 20) {
     setColor(color[index]);
-    prevCount = 0;
+    count = 0;
     index--;
     if (index < 0) {
       index = color.length - 1;
@@ -17,11 +22,16 @@ export function prevColor(setColor) {
 }
 
 export function nextColor(setColor) {
-  nextCount++;
+  if (gesture === "Thumb_Up") {
+    count++;
+  } else {
+    gesture = "Thumb_Up";
+    count = 0;
+  }
 
-  if (nextCount > 20) {
+  if (count > 20) {
     setColor(color[index]);
-    nextCount = 0;
+    count = 0;
     index++;
     if (index > color.length - 1) {
       index = 0;
